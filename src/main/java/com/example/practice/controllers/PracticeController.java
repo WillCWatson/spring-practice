41df65a3f237f84a9049a64f5d430de6f8c1a3bf
William Watson <williamcwatson@protonmail.com>
2021-04-15 15:18:24 -0400

    Made a change to the imports because they aren't working for some reason

--- src/main/java/com/example/practice/controllers/PracticeController.java
+++ src/main/java/com/example/practice/controllers/PracticeController.java
@@ -1,7 +1,7 @@
 package com.example.practice.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PracticeController {
